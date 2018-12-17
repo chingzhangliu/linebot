4705d41b1c0e37da4b2636a85f2a891f255da7a8
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2018-12-17 14:54:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,10 @@ def find_bookls(kw):
     return ans
 
 def loadPMJson():
-    
+    with urllib.request.urlopen("https://opendata.epa.gov.tw/ws/Data/ATM00625/?$format=json") as url:
+        data = json.load(url.read().decode())
+        for ele in data:
+            pm_site[ele['SiteName']] = ele['PM2.5']
 
 def getCls(cls_prefix):
     ret_cls = []
